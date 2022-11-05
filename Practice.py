cba80c431e51adbb2d4d8fa545cf6c941e420729
Rich Melendez <richardstevenmelendez@gmail.com>
2022-11-05 00:17:45 -0400

    First Commit

---
+++ Practice.py
@@ -0,0 +1,9 @@
+# This program calculates volume
+
+length = int(input("Enter a length amount: "))
+width = int(input("Enter a width amount: "))
+height = int(input("Enter a height amount: "))
+
+volume = length * width * height
+print(f"The calculated volume is: {volume:,.2f}")
+
